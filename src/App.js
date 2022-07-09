d421ab44b9bb13caa9b3227fd1db3ee1afce986a
Prateek Rath <prateekrath28@gmail.com>
2022-07-09 21:40:30 +0530

    Delete feedbacks

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import feedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 
 function App() {
-    const [feedback,setFeedback]=useState(feedbackData)
+    const [feedback, setFeedback] = useState(feedbackData)
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter(item=>item.id!==id))
+        }
+    }
   return (
     <>
       <Header />
       <div className="container">
-              <FeedbackList key={1} feedbacks={ feedback} />
+              <FeedbackList key={1} feedbacks={feedback} handleDelete={ deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,14 +1,14 @@
 import FeedbackItem from "./FeedbackItem"
 import PropTypes from 'prop-types'
 
-function FeedbackList({ feedbacks }) {
+function FeedbackList({ feedbacks,handleDelete }) {
     if (!feedbacks || feedbacks.length === 0) {
         return <p>No feedback yet</p>
     }
   return (
       <div className="feedback-list">
           {feedbacks.map(feedback => (
-              <FeedbackItem key={feedback.id} item={feedback}/>
+              <FeedbackItem key={feedback.id} item={feedback} handleDelete={handleDelete } />
           ))}
     </div>
   )
