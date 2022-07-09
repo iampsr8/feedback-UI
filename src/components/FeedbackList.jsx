9604bf08ed12a22ba5435e4d99154ba0d6dbd880
Prateek Rath <prateekrath28@gmail.com>
2022-07-10 01:52:34 +0530

    add animations

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,21 +1,30 @@
-import FeedbackItem from "./FeedbackItem"
-import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from "framer-motion";
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
 
-function FeedbackList({ feedbacks,handleDelete }) {
-    if (!feedbacks || feedbacks.length === 0) {
-        return <p>No feedback yet</p>
-    }
+function FeedbackList({ feedbacks, handleDelete }) {
+  if (!feedbacks || feedbacks.length === 0) {
+    return <p>No feedback yet</p>;
+  }
   return (
-      <div className="feedback-list">
-          {feedbacks.map(feedback => (
-              <FeedbackItem key={feedback.id} item={feedback} handleDelete={handleDelete } />
-          ))}
+    <div className="feedback-list">
+      <AnimatePresence>
+        {feedbacks.map((feedback) => (
+          <motion.div key={feedback.id} initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}}>
+            <FeedbackItem
+              key={feedback.id}
+              item={feedback}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
-  )
+  );
 }
 
 FeedbackList.propTypes = {
-    feedbacks:PropTypes.array
-}
+  feedbacks: PropTypes.array,
+};
 
-export default FeedbackList
+export default FeedbackList;
