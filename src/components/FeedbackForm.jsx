e849baddc454e25f17692c45b888f6aa4edd9bad
Prateek Rath <prateekrath28@gmail.com>
2022-07-10 00:43:03 +0530

    Real time text validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,18 +4,31 @@ import Button from "./shared/Button"
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
     const handleTextChange = (e) => {
+        if (text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text must be atleast 10 characters')
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
     return (
         <Card>
             <form >
-                <h2>How would you rate our product</h2>
+                <h2>How would you rate our product?</h2>
             </form>
             <div className="input-group">
                 <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-                <Button type="submit" >Send</Button>
+                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
             </div>
+            {message && <div className="message">{ message}</div>}
       </Card>
     
   )
